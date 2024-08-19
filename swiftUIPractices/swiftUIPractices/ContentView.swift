9652f49e6ac99921bf792d6a498d45281e26806e
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-08-19 17:38:53 +0200

    Basic Counter app

--- swiftUIPractices/swiftUIPractices/ContentView.swift
+++ swiftUIPractices/swiftUIPractices/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count: Int = 0
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, SwiftUI!")
-            Circle()
-            Rectangle()
-            Button("Press me") {
-                print("Button tapped")
+        HStack {
+            ZStack {
+                Circle()
+                    .fill(Color.yellow)
+                Button("Increment") {
+                    self.count += 1
+                }
+            }
+            .frame(width: 100, height: 100)
+            
+            ZStack {
+                Circle()
+                    .fill(Color.gray)
+                Button("Reset") {
+                    self.count = 0
+                }
+            }
+            .frame(width: 100, height: 100)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Count: \(count)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.gray)
+                Text("Daniel, 2024")
+
             }
         }
         .padding()
