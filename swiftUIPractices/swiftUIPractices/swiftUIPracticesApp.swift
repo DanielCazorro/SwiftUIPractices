ee310bdb3c98e12e73a3a63fa9478f4e24d3ef4d
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-08-19 14:16:39 +0200

    First add: Creating project

---
+++ swiftUIPractices/swiftUIPractices/swiftUIPracticesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticesApp.swift
+//  swiftUIPractices
+//
+//  Created by Daniel Cazorro Frías on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
