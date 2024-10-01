8333b460070cc5cbab0597da1c6cac1bd024d7ac
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-10-01 18:19:12 +0200

    Initial commit

---
+++ AppProjects/VacationInMalaga/VacationInMalaga/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  VacationInMalaga
+//
+//  Created by Daniel Cazorro Frías on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AppProjects/VacationInMalaga/VacationInMalaga/VacationInMalagaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInMalagaApp.swift
+//  VacationInMalaga
+//
+//  Created by Daniel Cazorro Frías on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInMalagaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
