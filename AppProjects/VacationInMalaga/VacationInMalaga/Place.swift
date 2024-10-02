b072c13c4d1347a1ab0c674df89243b0fb6beb9c
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-10-02 19:16:16 +0200

    Basic MapKit examples

---
+++ AppProjects/VacationInMalaga/VacationInMalaga/Place.swift
@@ -0,0 +1,36 @@
+//
+//  Place.swift
+//  VacationInMalaga
+//
+//  Created by Daniel Cazorro Frías on 2/10/24.
+//
+
+import SwiftData
+import SwiftUI
+import MapKit
+
+@Model
+class Place {
+    #Unique<Place>([\.name, \.latitude, \.longitude])
+    
+    @Attribute(.unique) var name: String
+    var latitude: Double
+    var longitude: Double
+    var interested: Bool
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var image: Image {
+        Image(name.lowercased().replacingOccurrences(of: " ", with: ""))
+    }
+    
+    // "Bellagio", "Treasure Island"
+    init(name: String, latitude: Double, longitude: Double, interested: Bool) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.interested = interested
+    }
+}
