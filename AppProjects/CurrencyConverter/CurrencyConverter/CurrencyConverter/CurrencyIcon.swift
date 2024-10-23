7bae47f8f954ccf851270258c1a543ca2b78df7e
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-10-23 20:49:44 +0200

    Add ForEach basic struct

--- AppProjects/CurrencyConverter/CurrencyConverter/CurrencyConverter/CurrencyIcon.swift
+++ AppProjects/CurrencyConverter/CurrencyConverter/CurrencyConverter/CurrencyIcon.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Currenty_Icon: View {
+struct CurrencyIcon: View {
     let currencyImage: ImageResource
     let currencyName: String
     
@@ -34,5 +34,5 @@ struct Currenty_Icon: View {
 }
 
 #Preview {
-    Currenty_Icon(currencyImage: .goldpiece, currencyName: "Gold Piece")
+    CurrencyIcon(currencyImage: .goldpiece, currencyName: "Gold Piece")
 }
