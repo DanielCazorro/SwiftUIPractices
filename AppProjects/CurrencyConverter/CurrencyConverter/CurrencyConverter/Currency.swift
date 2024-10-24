e6ddf919edd402165f192d7a3dd1e2ec5df9051b
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-10-24 10:56:42 +0200

    Add basic model structure

---
+++ AppProjects/CurrencyConverter/CurrencyConverter/CurrencyConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  CurrencyConverter
+//
+//  Created by Daniel Cazorro Frías on 24/10/24.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
