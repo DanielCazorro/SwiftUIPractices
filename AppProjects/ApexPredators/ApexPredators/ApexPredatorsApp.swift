f28c284dbd73c599cab879ef8317cb21e57494dc
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-11-11 13:24:20 +0100

    Create basic app's structure

---
+++ AppProjects/ApexPredators/ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Daniel Cazorro Frías on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
