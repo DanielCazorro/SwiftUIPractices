4ee7c23dea113dc93d7eff2f9e1351c5a878d101
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-11-14 19:04:21 +0100

    Create Apex List, with models, shadow and strings modifications

--- AppProjects/ApexPredators/ApexPredators/ApexPredator.swift
+++ AppProjects/ApexPredators/ApexPredators/ApexPredator.swift
@@ -6,17 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct ApexPredator: Decodable {
+struct ApexPredator: Decodable, Identifiable {
     let id: Int
-    let name, type: String
+    let name: String
+    let type: PredatorType
     let latitude, longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
-    let links: String
+    let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
     
     struct MovieScene: Decodable {
         let id: Int
         let movie, sceneDescription: String
+    }
+    
+    enum PredatorType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
     }
 }
