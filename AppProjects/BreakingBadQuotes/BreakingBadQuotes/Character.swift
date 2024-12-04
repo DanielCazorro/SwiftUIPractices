2ab96470319e51abf2c17d8c83d1b4783d999d4e
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-12-04 18:04:10 +0100

    Add basic models for api json information

---
+++ AppProjects/BreakingBadQuotes/BreakingBadQuotes/Character.swift
@@ -0,0 +1,18 @@
+//
+//  Character.swift
+//  BreakingBadQuotes
+//
+//  Created by Daniel Cazorro Frías on 4/12/24.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    let name: String
+    let birthday: String
+    let occupations: [String]
+    let images: [URL]
+    let aliases: [String]
+    let status: String
+    let portrayedBy: String
+}
