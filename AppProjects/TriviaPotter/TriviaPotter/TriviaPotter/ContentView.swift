7d2fda41f0a74f94e6342466877abd33b32eebd1
Daniel Cazorro Frías <danielcazorro@outlook.com>
2025-01-07 15:46:19 +0100

    Create basic structure for the app

---
+++ AppProjects/TriviaPotter/TriviaPotter/TriviaPotter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriviaPotter
+//
+//  Created by Daniel Cazorro Frías on 7/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AppProjects/TriviaPotter/TriviaPotter/TriviaPotter/TriviaPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaPotterApp.swift
+//  TriviaPotter
+//
+//  Created by Daniel Cazorro Frías on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
