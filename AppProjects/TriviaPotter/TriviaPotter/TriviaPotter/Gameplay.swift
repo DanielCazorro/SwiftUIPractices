82d9461cf6039131c2853ffe9d740475a2bae952
Daniel Cazorro Frías <danielcazorro@outlook.com>
2025-01-13 17:34:50 +0100

    Create basic function for questions of the game

--- AppProjects/TriviaPotter/TriviaPotter/TriviaPotter/Gameplay.swift
+++ AppProjects/TriviaPotter/TriviaPotter/TriviaPotter/Gameplay.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct Gameplay: View {
     @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var game: Game
     @Namespace private var namespace
     @State private var musicPlayer: AVAudioPlayer!
     @State private var sfxPlayer: AVAudioPlayer!
@@ -353,4 +354,5 @@ struct Gameplay: View {
 
 #Preview {
     Gameplay()
+        .environmentObject(Game())
 }
